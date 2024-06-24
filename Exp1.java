393222a515df9d7f33a6cb1639f3a9f7d8f64899
unknown <archanapogula14@gmail.com>
2024-06-24 15:54:05 +0530

    First Commit

---
+++ Exp1.java
@@ -0,0 +1,12 @@
+Import java.util.Scanner;
+public class Exp1{
+  public static void main(String[]args){
+  int a,b,c;
+  Scanner sc=New Scanner(system.in);
+  System.out.println("enter 2 numbers");
+  a=sc.nextInt();
+  b=sc.nextInt();
+  c=a/b;
+  system.out.println();
+ }
+}
